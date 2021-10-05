9f7ee22b1b045ca8199892c0d3dbce5ff9c72d55
Carlos Eduardo Santiago <deduardo_6@hotmail.com>
2021-10-05 15:05:41 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Carlos Eduardo Santiago on 05/10/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
