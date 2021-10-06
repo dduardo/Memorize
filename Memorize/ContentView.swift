22277ae25f8c7a583ef5a9ccf182fbbe94092a55
Carlos Eduardo Santiago <deduardo_6@hotmail.com>
2021-10-05 22:01:04 -0300

    Feature - Lecture 2 Learning More about SwiftUI,  creating reusable components (a Card in the game) and combining them to make more complex user-interfaces.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,15 +7,80 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct CardView: View {
+    var content: String
+    @State var isFaceUp: Bool = true
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            self.isFaceUp = !self.isFaceUp
+        }
+    }
+}
+
+struct ContentView: View {
+    var emojis = ["🚀", "✈️" ,"🚁", "🚘", "🛳", "🚗", "🚙", "🚔", "🚎","🏎","🚓","🚒","🛻","🚜", "🚍", "🚖", "🚂", "🛩", "🛸", "⛵️", "⛴", "🛺", "🚚", "🚐"]
+    @State var emojiCount = 20
+    
+    var body: some View {
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65 ))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }
+            .foregroundColor(.red)
+            Spacer()
+            HStack {
+                remove
+                Spacer()
+                add
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
+        }
+        .padding(.horizontal)
+    }
+    
+    var remove: some View {
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+        }
+    }
+    
+    var add: some View {
+        Button {
+            if emojiCount < emojis.count{
+                emojiCount += 1
+            }
+        } label: {
+            Image(systemName: "plus.circle")
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
